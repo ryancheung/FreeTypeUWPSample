77393d7dbc2eebc0959fb6a6c81ef91e07ff0323
ryancheung <ryancheung.go@gmail.com>
2020-08-30 22:45:45 +0800

    Move native libs to a class library to check if it works

--- App1/MainPage.xaml.cs
+++ App1/MainPage.xaml.cs
@@ -13,6 +13,7 @@ using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 using System.Runtime.InteropServices;
+using ClassLibrary1;
 
 // https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x804 上介绍了“空白页”项模板
 
@@ -23,41 +24,15 @@ namespace App1
     /// </summary>
     public sealed partial class MainPage : Page
     {
-        [DllImport("freetype", CallingConvention = CallingConvention.Cdecl)]
-        public static extern int FT_Init_FreeType(out IntPtr alibrary);
 
-        [DllImport("kernel32", CallingConvention = CallingConvention.Cdecl)]
-        public static extern int LoadPackagedLibrary([MarshalAs(UnmanagedType.LPWStr)] string libFileName, int reserved = 0);
-
-        [DllImport("kernel32")]
-        public static extern uint GetLastError();
 
         public MainPage()
         {
             this.InitializeComponent();
 
-            string archPrefix = "uwp-x86";
+            NativeInvokes.Init();
 
-            switch (RuntimeInformation.ProcessArchitecture)
-            {
-                case Architecture.X86:
-                    archPrefix = "uwp-x86";
-                    break;
-                case Architecture.X64:
-                    archPrefix = "uwp-x64";
-                    break;
-                case Architecture.Arm:
-                case Architecture.Arm64:
-                    archPrefix = "uwp-arm";
-                    break;
-            }
-
-            LoadPackagedLibrary(@"runtimes\" + archPrefix + @"\zlib1.dll");
-            LoadPackagedLibrary(@"runtimes\" + archPrefix + @"\brotlicommon.dll");
-            LoadPackagedLibrary(@"runtimes\" + archPrefix + @"\brotlidec.dll");
-            LoadPackagedLibrary(@"runtimes\" + archPrefix + @"\freetype.dll");
-
-            var ret = FT_Init_FreeType(out var lib);
+            var ret = NativeInvokes.FT_Init_FreeType(out var lib);
             System.Diagnostics.Debug.WriteLine("arch: {2} - ret: {0}, lib: {1}", ret, lib, RuntimeInformation.ProcessArchitecture);
         }
     }
---
+++ ClassLibrary1/NativeInvokes.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1
+{
+    public class NativeInvokes
+    {
+        [DllImport("freetype", CallingConvention = CallingConvention.Cdecl)]
+        public static extern int FT_Init_FreeType(out IntPtr alibrary);
+
+        [DllImport("kernel32", CallingConvention = CallingConvention.Cdecl)]
+        public static extern int LoadPackagedLibrary([MarshalAs(UnmanagedType.LPWStr)] string libFileName, int reserved = 0);
+
+        [DllImport("kernel32")]
+        public static extern uint GetLastError();
+
+        public static void Init()
+        {
+            string archPrefix = "uwp-x86";
+
+            switch (RuntimeInformation.ProcessArchitecture)
+            {
+                case Architecture.X86:
+                    archPrefix = "uwp-x86";
+                    break;
+                case Architecture.X64:
+                    archPrefix = "uwp-x64";
+                    break;
+                case Architecture.Arm:
+                case Architecture.Arm64:
+                    archPrefix = "uwp-arm";
+                    break;
+            }
+
+            int ret;
+            var moduelName = typeof(NativeInvokes).Assembly.GetName().Name;
+            ret = LoadPackagedLibrary(moduelName + @"\runtimes\" + archPrefix + @"\zlib1.dll");
+            ret = LoadPackagedLibrary(moduelName + @"\runtimes\" + archPrefix + @"\brotlicommon.dll");
+            ret = LoadPackagedLibrary(moduelName + @"\runtimes\" + archPrefix + @"\brotlidec.dll");
+            ret = LoadPackagedLibrary(moduelName + @"\runtimes\" + archPrefix + @"\freetype.dll");
+        }
+    }
+}
